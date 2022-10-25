6d55f6ab4cc88fff4682854557ecfc4e62d009b6
Christian Manzaraz <magnusmanz@gmial.com>
2022-10-25 19:03:06 -0300

    agregar botones mezcladores y actualizar codigo

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -9,19 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var colorView: UIView!
+    @IBOutlet var redSwitch: UISwitch!
+    @IBOutlet var greenSwitch: UISwitch!
+    @IBOutlet var blueSwitch: UISwitch!
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        if redSwitch.isOn {
+            red = 1
+        }
+        if greenSwitch.isOn {
+            green = 1
+        }
+        if blueSwitch.isOn {
+            blue = 1
+        }
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        colorView.backgroundColor = .black
+        updateColor()
     }
 
     @IBAction func switchChanged(_ sender: UISwitch) {
-        if sender.isOn {
-            colorView.backgroundColor = .red
-        } else {
-            colorView.backgroundColor = .black
-        }
+        updateColor()
     }
     
 }
