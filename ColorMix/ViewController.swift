0f46ca41070c453ff8247ac459f63d6da2f25fd2
Christian Manzaraz <magnusmanz@gmial.com>
2022-10-25 22:19:01 -0300

    adding sliders

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -12,32 +12,38 @@ class ViewController: UIViewController {
     @IBOutlet var redSwitch: UISwitch!
     @IBOutlet var greenSwitch: UISwitch!
     @IBOutlet var blueSwitch: UISwitch!
-    func updateColor() {
-        var red: CGFloat = 0
-        var green: CGFloat = 0
-        var blue: CGFloat = 0
-        if redSwitch.isOn {
-            red = 1
-        }
-        if greenSwitch.isOn {
-            green = 1
-        }
-        if blueSwitch.isOn {
-            blue = 1
-        }
-        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
-        colorView.backgroundColor = color
-    }
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        updateColor()
     }
 
     @IBAction func switchChanged(_ sender: UISwitch) {
         updateColor()
     }
     
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        if redSwitch.isOn {
+            red = CGFloat(redSlider.value)
+        }
+        if greenSwitch.isOn {
+            green = CGFloat(greenSlider.value)
+        }
+        if blueSwitch.isOn {
+            blue = CGFloat(blueSlider.value)
+        }
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+    }
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+        updateColor()
+    }
 }
 
